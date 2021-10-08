1326299365c80ccca914ab5a0d0d0fc086bc8ddb
steve02081504 <steve02081504@EDEN.fukwold>
2021-10-08 14:44:08 +0800

    fix

--- check-tool.cpp
+++ check-tool.cpp
@@ -1,9 +1,16 @@
 #include "my-gists/ukagaka/shiori_loader.hpp"
 #include <cstdlib>
+#include <cstdio>
 
 Cshiori shiori;
 
 int wmain(int argc,wchar_t**argv){
-	shiori.SetTo(argv[0]);
-	return shiori.yaya_CI_check_failed()?EXIT_FAILURE:EXIT_SUCCESS;
+	wchar_t shiori_path[_MAX_PATH]=L"";
+	_wfullpath(shiori_path, argv[1], _MAX_PATH);
+	shiori.SetTo(shiori_path);
+	auto failed=shiori.yaya_CI_check_failed();
+	if (failed){
+		fprintf(stderr,"::error");
+	}
+	return failed?EXIT_FAILURE:EXIT_SUCCESS;
 };
