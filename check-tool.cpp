6349689964316785fd567148525405679ad1b163
steve02081504 <steve02081504@EDEN.fukwold>
2021-10-08 16:07:27 +0800

    err msg

--- check-tool.cpp
+++ check-tool.cpp
@@ -5,12 +5,19 @@
 Cshiori shiori;
 
 int wmain(int argc,wchar_t**argv){
+	//::error file={name},line={line},endLine={endLine},title={title}::{message}
+	//::warning file={name},line={line},endLine={endLine},title={title}::{message}
+	//::notice file={name},line={line},endLine={endLine},title={title}::{message}
+	//
 	wchar_t shiori_path[_MAX_PATH]=L"";
-	_wfullpath(shiori_path, argv[1], _MAX_PATH);
+	if (!_wfullpath(shiori_path, argv[1], _MAX_PATH))
+		fprintf(stderr,"::error title={can't get fullpath of shiori}::{_wfullpath returns NULL}");
 	shiori.SetTo(shiori_path);
+	if(!shiori.All_OK())
+		fprintf(stderr,"::error title={shiori load failed}::{shiori.ALL_OK() returns false}");
 	auto failed=shiori.yaya_CI_check_failed();
 	if (failed){
-		fprintf(stderr,"::error");
+		fprintf(stderr,"::error title={some error in your dic}::{open your tama!}");
 	}
 	return failed?EXIT_FAILURE:EXIT_SUCCESS;
 };
