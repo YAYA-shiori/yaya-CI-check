bd491e4610321bf52c2f12bd621d0ae8d428c4a3
steve02081504 <steve02081504@EDEN.fukwold>
2021-10-08 22:09:24 +0800

    remove get fullpath

--- check-tool.cpp
+++ check-tool.cpp
@@ -9,10 +9,7 @@ int wmain(int argc,wchar_t**argv){
 	//::warning file={name},line={line},endLine={endLine},title={title}::{message}
 	//::notice file={name},line={line},endLine={endLine},title={title}::{message}
 	//
-	wchar_t shiori_path[_MAX_PATH]=L"";
-	if (!_wfullpath(shiori_path, argv[1], _MAX_PATH))
-		fprintf(stderr,"::error title=_wfullpath returns NULL::can't get fullpath of shiori");
-	shiori.SetTo(shiori_path);
+	shiori.SetTo(argv[1]);
 	if(!shiori.All_OK())
 		fprintf(stderr,"::error title=shiori.ALL_OK() returns false::shiori load failed");
 	if(!shiori.can_make_CI_check())
